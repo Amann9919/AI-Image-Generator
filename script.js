79f3f8cb08f5f878bc4bfb7f769cdf05d753cd85
Aman <amanmaurya16517@gmail.com>
2025-07-15 18:25:22 +0530

    commit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const gridGallery = document.querySelector(".gallery-grid");
 const errorMsg = document.getElementById("errorMsg");
 
 // Hugging face api key
-const API_KEY = "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";
+const API_KEY = "hf_JIhLuSWtQqCNwNDLcIFVtQWFYdzYOuRMZK";
 
 const examplePrompts = [
   "A magic forest with glowing plants and fairy homes among giant mushrooms",
